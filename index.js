fb2d761502aade898f6251aad0d95da0bf6afd49
Dominic Tarr <dominic.tarr@gmail.com>
2012-08-05 15:40:22 +0200

    fix syntax error

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-var argv = require('optimist').argv)
+var argv = require('optimist').argv
 var cc   = require('config-chain')
 var join = require('path').join
 var home = process.env.HOME
 var etc = '/etc'
 
-
+console.log(argv)
 module.exports = function (name, defaults) {
   if(!name)
     throw new Error('nameless configuration fail')
 
   return cc(
     argv,
-    cc.env(name + '_')
+    cc.env(name + '_'),
     argv.config,
     join(home, '.' + name + 'rc'),
     join(home, '.' + name, 'config'),
@@ -21,6 +21,6 @@ module.exports = function (name, defaults) {
     join(etc, name + 'rc'),
     join(etc, name, 'config'),
     defaults
-  ).store
+  ).snapshot
 
 }
